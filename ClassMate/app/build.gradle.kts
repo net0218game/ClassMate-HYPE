a7e523f96825bbcc22b78dadd74f14652ba57b57
DⱯɅID <game.net0218@gmail.com>
2024-01-11 10:32:15 +0100

    CalendarView Library

--- ClassMate/app/build.gradle.kts
+++ ClassMate/app/build.gradle.kts
@@ -55,6 +55,10 @@ dependencies {
 
     implementation("com.github.yukuku:ambilwarna:2.0.1")
 
+    // Calendar
+    implementation ("com.github.prolificinteractive:material-calendarview:2.0.0")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
